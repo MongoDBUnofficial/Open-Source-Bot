770c06f89d4d755ee556250f86915ca10bd125cd
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-11 21:13:51 +0100

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -11,11 +11,22 @@ module.exports = {
 
         const roleColor = message.guild.me.displayHexColor;
 
+        let seconds = ms / 1000;
+        ms = (seconds % 1) * 1000;
+        seconds -= (ms / 1000);
+        let minutes = seconds / 60;
+        seconds = (minutes % 1) * 60;
+        minutes -= (seconds / 60);
+        let hours = minutes / 60;
+        minutes = (hours % 1) * 60;
+        hours -= (minutes / 60);
+        let time = ""+hours+":"+minutes+":"+seconds+"."+ms;
+
         const embed = new MessageEmbed()
 
     .setColor(roleColor)
     .setTitle("Uptime")
-    .setDescription(client.uptime)
+    .setDescription(time)
     .setTimestamp()
 
     message.channel.send(embed)
