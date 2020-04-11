eeec72d35af20ddc49b585de6167b407bcc09d8b
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-11 06:25:49 +0100

    'Undefined' error fix

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -14,7 +14,7 @@ module.exports = {
         const roleColor = message.guild.me.displayHexColor;
 
         message.channel.send(new MessageEmbed()
-        .setFooter(message.author.name)
+        .setFooter(message.author.tag)
         .setColor(roleColor)
         .setDescription(args.join(" ")));
         
