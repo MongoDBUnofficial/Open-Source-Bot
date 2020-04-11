3a6008039f10b700e33578263056b98881d021e9
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-11 07:25:24 +0100

    Report.js command added

---
+++ commands/moderation/report.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require("common-tags")
+
+module.exports = {
+    name: "report",
+    category: "moderation",
+    description: "Reports a member",
+    usage: "<mention | id>",
+    run: async ( client,message, args) => {
+
+        if (message.deletable) message.delete();
+    
+let rMember = message.mentions.members.first() || message.guild.members.get(ards[0]);
+
+if (!rMember)
+return message.reply("Oops! I couldn't find that person!").then(m => m.delete(5000));
+
+if (rMember.hasPermission("BAN_MEMBERS") || rMember.user.bot)
+return message.reply ("You can't report that member!")
+
+if (!args[1])
+return message.channel.send("Please provide a reason for the report!").then(m.delete(5000));
+
+const channel = message.guild.channels.find(channel => channel.name === "reports");
+
+if (!channel)
+return message.channel.send("This server isn't set up correctly for reports, please create a #reports channel!")
+
+const embed = new MessageEmbed()
+.setColor("#ff0000")
+.setTimestamp()
+.setFooter(message.guild.name, message.guild.iconURL)
+.setAuthor("Reported member", rMember.user.displayAvatarURL)
+.setDescription(stripIndents`**> Reported Member:** ${rMember}
+**> Reported by:** ${message.member} (${message.member.id})
+**> Reported in:** ${message.channel}
+**> Reason:** ${args.slice(1).join(" ")}`)
+
+channel.send(embed)
+
+    }
+
+}
