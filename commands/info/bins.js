13980b859737ac5369e01554c6d72306bd0d5852
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-11 01:49:52 +0100

    Update bins.js

--- commands/info/bins.js
+++ commands/info/bins.js
@@ -11,7 +11,7 @@ module.exports = {
 
     .setFooter(message.author.tag)
     .setThumbnail(message.author.AvatarURL)
-    .setColor(message.author.displayHexColor === "#000000" ? "#ffffff" : message.author.displayHexColor)
+    .setColor(roleColor)
     .addField("**Reccomended bin:**", "https://sourceb.in/")
     .setTimestamp()
 
