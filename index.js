dd0d78bd66cb40eecb8103b6833b7bbdb6af7c17
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-11 01:05:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+ disableEveryone: true
+});
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+//Config
+require("dotenv").config({
+    path: __dirname + "/.env"
+});
+
+["command"].forEach(handler => {
+
+require(`./handler/${handler}`)(client);
+
+});
+
+//Ready
+client.on("ready", () => {
+
+    console.log(`${client.user.username} is ready!`) + client.user.setStatus("online"); 
+  
+    let statuses = [
+     `Game News`,
+     `Meme Generators`,
+     `the ATM`
+    ];
+    
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {type: "LISTENING"})
+    }, 2500);
+
+});
+
+client.on("message", async message => {
+    const prefix = "c!";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd.length === 0) return;
+
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    if (command) command.run(client, message, args);
+
+});
+
+client.login(process.env.TOKEN);
