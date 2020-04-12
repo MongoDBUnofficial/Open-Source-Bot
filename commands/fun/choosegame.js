315afe71a893ba478a0deeda189bf384f3d3dea5
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-13 00:34:07 +0100

    Update choosegame.js

--- commands/fun/choosegame.js
+++ commands/fun/choosegame.js
@@ -10,30 +10,36 @@ module.exports = {
 
         if (message.deletable) message.delete(); 
     
+const arrayOfChoice = [
+    "https://jackboxgames.com/",
+    "http://slither.io/",
+    "https://hole-io.com/",
+    "http://www.totaljerkface.com/happy_wheels.tjf",
+    "http://www.treasurearena.com/",
+    "https://www.roblox.com/",
+    "http://www.quakelive.com/",
+    "https://www.blackmesasource.com/",
+    "https://www.battlefield.com/battlefield-1942",
+    "https://www.starwars.com/video/star-wars-commander-worlds-in-conflict-official-trailer",
+    "https://www.kongregate.com/games/graebor/sort-the-court",
+    "https://plarium.com/en/strategy-games/throne-kingdom-at-war/game/",
+    "https://www.warframe.com/",
+    "https://deepart.io/"
+];
+function getChoice() {
+    return arrayOfChoices[Math.round(Math.random() * arrayOfChoices.length)];
+  }
+  
+let count = 0;
+if(count >= links.length) count = 0;
 
+const embed = new MessageEmbed()
+.setTitle("Random Game Generator")
+.setDescription(getChoice());
 
-        const links = [
-            "https://jackboxgames.com/",
-            "http://slither.io/",
-            "https://hole-io.com/",
-            "http://www.totaljerkface.com/happy_wheels.tjf",
-            "http://www.treasurearena.com/",
-            "https://www.roblox.com/",
-            "http://www.quakelive.com/",
-            "https://www.blackmesasource.com/",
-            "https://www.battlefield.com/battlefield-1942",
-            "https://www.starwars.com/video/star-wars-commander-worlds-in-conflict-official-trailer",
-            "https://www.kongregate.com/games/graebor/sort-the-court",
-            "https://plarium.com/en/strategy-games/throne-kingdom-at-war/game/",
-            "https://www.warframe.com/",
-            "https://deepart.io/"
-        ];
-      
-        let count = 0;
-        if(count >= links.length) count = 0;
-        message.channel.send(links[count]);
-        count++;
-      
+message.channel.send(embed)
+
+count++;
 
     }
 }
