268319707118f1116e0759445f674559c70962a9
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-28 01:28:27 +0100

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -7,7 +7,7 @@
             description: "Activates the meme machine 🖨.",
             run: async (client, message, args) => {
 
-                const subReddits = ["dankmeme", "meme", "me_irl", "PewdiepieSubmissions"];
+                const subReddits = ["dankmeme", "meme", "me_irl", "PewdiepieSubmissions", "DankMemesFromSite19"];
 
                 const random = subReddits[Math.floor(Math.random() * subReddits.length)];
         
