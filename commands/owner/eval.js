80c222cb2104e6be702376dc3a90b6c4d0349d0c
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-28 23:34:56 +0100

    Create eval.js

---
+++ commands/owner/eval.js
@@ -0,0 +1,19 @@
+const { util } = require("util");
+
+module.exports.run = (client, message, args) => {
+
+if (!message.author.id === "324593687369744385") {
+    message.reply("You are not the bot owner!")
+    return;
+}
+
+	if (!args[0]) return message.reply("you must provide JavaScript code!");
+    
+    try {
+        let output = eval(args.join(" "));
+        if (typeof output !== "string") output = util.inspect(output);
+        return message.channel.send(`**Output:**\n\`\`\`js\n${output}\n\`\`\``);
+    } catch(err) {
+        return message.channel.send(`**Error:**\n\`${err}\``);
+    }
+}
