d0d692729a52d903283d510422ad68a0a6d9578e
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 00:55:31 +0100

    Install underscore and eval command

---
+++ commands/DEVELOPER ONLY/eval.js
@@ -0,0 +1,36 @@
+var _ = require("underscore");
+
+module.exports = {
+    name: "eval",
+    category: "owner",
+    description: "[DEVELOPER ONLY]",
+    run: async ( client,message, args) => {
+
+        if(message.author.user.id !== 324593687369744385) return;
+
+        var params = message.content.split(" ");
+        params = _.rest(params, 1);
+        var cmd = "";
+        params.forEach(param =>
+        {
+            cmd = cmd + " " +param;
+        });
+        cmd.replace("require(", "");
+        cmd.replace("exec(", "");
+        var mstr = "";
+        console.log(message.author.username+"#"+message.author.discriminator+" ran exec on: "+cmd);
+        try
+        {
+            mstr = eval(cmd);
+        }
+        catch(e)
+        {
+            mstr = e;
+        }
+        if(mstr != "")
+        {
+            message.channel.send(mstr);
+        }
+
+    }
+}
