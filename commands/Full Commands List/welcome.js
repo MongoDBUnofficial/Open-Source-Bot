02b83f777a66ea84fce4dd0de658299e257bef62
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 21:27:53 +0100

    Update welcome.js

--- commands/Full Commands List/welcome.js
+++ commands/Full Commands List/welcome.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "dashcraftwelc",
     description: "A FAQ For the Dashcraft discord server.",
     run: async ( client,message, args) => {
-
+message.delete()
 message.channel.send("Welcome to Dashcrafts Nation mod team USF and Voramu!")
 
     }
