e79e9b9274cb8316d17ac96996cdef192f991d6b
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 23:40:31 +0100

    Update dashcraftfaq.js

--- commands/Full Commands List/dashcraftfaq.js
+++ commands/Full Commands List/dashcraftfaq.js
@@ -49,8 +49,7 @@ const herokuEmbed = new MessageEmbed()
 .setTitle("Heroku Help")
 .setDescription(`First, make sure your profile says
 \`\`\`Worker: node index.js\`\`\`
-After that, do the following.
-\`\`\`Go to Deploy and scroll to the bottom and click Deploy Branch.\`\`\`
+**After that, do the following.**
 After that, go to deploy tab on Heroku, scroll down and make sure automatic deploy is on. after making sure about that,
 Click on deploy branch on bottom. it should check all of the build steps, if it didn't happened,
 means there is something wrong with your code or your setup, contact us at support and we will help you to fix it.
