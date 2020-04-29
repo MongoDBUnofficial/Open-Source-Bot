5ea373691f779f6fa8deafb1623f5032e0d99ed2
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 13:09:36 +0100

    Update emojis.js

--- commands/fun/emojis.js
+++ commands/fun/emojis.js
@@ -29,6 +29,7 @@ module.exports = {
         let Embed = new MessageEmbed()
         .setTitle(`All emojis in ${message.guild.name}.`)
         .setDescription(`**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**Total: ${OverallEmojis}**`)
+        .setThumbnail(message.guild.iconURL)
         .setColor(`RANDOM`)
         message.channel.send(Embed)
 
