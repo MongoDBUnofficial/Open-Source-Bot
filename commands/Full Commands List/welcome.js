b4cf1f1b91c63de3c4cc0ce41ac689b102ad9142
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 21:28:58 +0100

    Delete welcome.js

--- commands/Full Commands List/welcome.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "dashcraftwelc",
-    description: "A FAQ For the Dashcraft discord server.",
-    run: async ( client,message, args) => {
-message.delete()
-message.channel.send("Welcome to Dashcrafts Nation mod team USF and Voramu!")
-
-    }
-}
