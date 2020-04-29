cb73b28b0cead83c2c554e0ac4ec714c79b0c1c7
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 21:23:12 +0100

    Create welcome.js

---
+++ commands/Full Commands List/welcome.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "dashcraftwelc",
+    description: "A FAQ For the Dashcraft discord server.",
+    run: async ( client,message, args) => {
+
+message.channel.send("Welcome to Dashcrafts Nation mod team USF and Voramu!")
+
+    }
+}
