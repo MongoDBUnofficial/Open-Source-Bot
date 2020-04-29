edd59a26c7036b7489678062d6198d1c1188bd1b
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 23:26:51 +0100

    Update dashcraftfaq.js

--- commands/Full Commands List/dashcraftfaq.js
+++ commands/Full Commands List/dashcraftfaq.js
@@ -14,6 +14,14 @@ message.reply("This command is only for a certain server. ")
 
         const promptEmbed = new MessageEmbed()
         .setTitle("Dashcraft FAQ")
+        .setThumbnail(message.guild.iconURL)
+        .addField(`**Select an emoji from the key
+        for more information about it.**`)
+        .addFields(
+            { name: "Who is DashCraft?", value: "🎟", inline: true},
+            { name: "What is this Discord server for?", value: "🦺", inline: true},
+            { name: "My Heroku isn't working. How do I fix it?", value: "🟪", inline: true},
+        )
         .setColor("GREEN")
         .setAuthor(`test`)
         .setDescription(`pick an emoji test`)
@@ -21,31 +29,49 @@ message.reply("This command is only for a certain server. ")
     // Send the message
     const m = await message.channel.send(promptEmbed).then(async msg =>{
         // Await the reactions and the reactioncollector
-        const emoji = await promptMessage(msg, message.author, 30, ["✅", ":heroku:705174731162255502"]);
+        const emoji = await promptMessage(msg, message.author, 30, ["🎟", "🟪", "🦺"]);
 
         // Verification stuffs
-        if (emoji === "✅") { 
+        if (emoji === "🎟") { 
            
             msg.delete()
 
             const dashcraftEmbed = new MessageEmbed()
-            .setTitle("Welcome to the Dashcraft Discord Server!")
-            .addField(`Members: ${message.guild.cache.users}`)
-            .addField(`Created: ${message.guild.createdAt}`)
+            .setTitle("Who is DashCraft?")
+            .addField("Dashcraft, or Poryafm12¹ on Discord is the creator of http://youtube.com/dashcraft123")
 
             message.channel.send(dashcraftEmbed)
 }
- else if (emoji === ":heroku:705174731162255502") {
+ else if (emoji === "🟪") {
 
 msg.delete()
 
 const herokuEmbed = new MessageEmbed()
 .setTitle("Heroku Help")
-.setDescription("test")
+.setDescription(`First, make sure your profile says
+\`\`\`Worker: node index.js\`\`\`
+After that, do the following.
+\`\`\`Go to Deploy and scroll to the bottom and click Deploy Branch.\`\`\`
+After that, wait a bit and then
+go to resouces and your dyno's should be there.
+If not, get help in the Support channel.`)
 
 message.channel.send(herokuEmbed)
 
         }
+        else if (emoji === "🦺") {
+
+            msg.delete()
+            
+            const herokuEmbed = new MessageEmbed()
+            .setTitle("What is this discord server for?")
+            .setDescription(`This server is for you to get
+            help with your code and the official chat server
+            for the DashCraft YouTube channel.`)
+            
+            message.channel.send(herokuEmbed)
+            
+                    }
     });
      
     }
