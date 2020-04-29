00e8f9936177c79472995e7dd2db191ba231c3a0
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 11:05:52 +0100

    Create botinfo.js

---
+++ commands/info/botinfo.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require("common-tags")
+
+module.exports = {
+    name: "botinfo",
+    category: "info",
+    description: "Returns with bot stats.",
+    run: async ( client,message, args) => {
+
+        if (message.deletable) message.delete(); 
+        const roleColor = message.guild.me.displayHexColor;
+
+        const botcreated = formatDate(client.user.createdAt)
+
+const embed = new MessageEmbed()
+.setTitle("Bot Information")
+.addFields(
+
+    { name : 'Users:', value : `${client.users.cache.size} users`},
+    { name : 'Servers:', value : `${client.guilds.cache.size} servers`},
+    { name : 'Tag:', value : `${client.user.tag}`, inline: true},
+    { name : 'Created At:', value : `${botcreated} `, inline: true},
+
+)
+.setColor(roleColor)
+.setDescription(getChoice());
+
+
+message.channel.send(embed)
+
+    }
+}
