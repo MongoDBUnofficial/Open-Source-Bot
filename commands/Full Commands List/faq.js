a209f58eb232bc120a9ada248301d2d5c05b8b50
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 21:02:08 +0100

    Update faq.js

--- commands/Full Commands List/faq.js
+++ commands/Full Commands List/faq.js
@@ -6,10 +6,10 @@ module.exports = {
     name: "dashcraftfaq",
     description: "A FAQ For the Dashcraft discord server.",
     run: async ( client,message, args) => {
-        if(message.guild.id !== 644764850706448384) {
+        if(message.guild.id !== '644764850706448384') {
 
 message.reply("This command is only for a certain server. ")
-
+ return;
         }
     }
 }
