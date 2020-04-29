be6690879583b004171f994350c462a06be5dd36
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-29 11:09:38 +0100

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const { stripIndents } = require("common-tags")
+const { formatDate } = require("../../functions.js")
 
 module.exports = {
     name: "botinfo",
