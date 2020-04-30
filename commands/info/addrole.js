6b24af635c8ad2394e17d47a9d54a06452c244ba
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-30 12:43:02 +0100

    Create addrole.js

---
+++ commands/info/addrole.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "addroletest",
+    category: "info",
+    description: "Where you can put large ammounts of text without spamming in the server.",
+    run: async ( client,message, args) => {
+
+message.member.addRole('698333678220083291')
+    }
+}
