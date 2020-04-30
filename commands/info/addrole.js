cc3f45a1b82f8a8eb9dc3f783882a2bf6f3db828
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-30 13:08:20 +0100

    Update addrole.js

--- commands/info/addrole.js
+++ commands/info/addrole.js
@@ -1,5 +1,4 @@
 const { GuildMemberRoleManager } = require("discord.js");
-const { roles } = require(GuildMemberRoleManager)
 
 
 module.exports = {
@@ -8,6 +7,7 @@ module.exports = {
     description: "Where you can put large ammounts of text without spamming in the server.",
     run: async ( client,message, args) => {
 
+        var roles = new GuildMemberRoleManager(roles)
         message.guildMember.roles.add('698333678220083291');
     }
 }
