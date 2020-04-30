c6af0c607605a0818ea4c2462f4e3bf01e2f2f9b
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-04-30 13:16:44 +0100

    Update addrole.js

--- commands/info/addrole.js
+++ commands/info/addrole.js
@@ -6,7 +6,7 @@ module.exports = {
     category: "info",
     description: "Where you can put large ammounts of text without spamming in the server.",
     run: async ( client,message, args) => {
-
+console.log(message)
         message.member.roles.add('698333678220083291');
     }
 }
