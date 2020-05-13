55fe11d8d7d975ecd464d13d5a50e6b8b07b97da
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-14 00:21:37 +0100

    DEBUG

--- commands/moderation/announce.js
+++ commands/moderation/announce.js
@@ -34,7 +34,7 @@ const embed = new MessageEmbed()
 .setAuthor(message.author.username , message.author.displayAvatarURL)
 .setDescription(`${args.slice().join(" ")}`)
 
-channel.send(embed)
+channel.send("@everyone" + embed)
 
 message.channel.send("Announcement made by user: " + message.author.username )
 
