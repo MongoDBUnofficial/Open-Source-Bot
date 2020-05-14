4d4b23bd32fbacbeeeb7815a78734c9554e9ccce
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-14 15:54:47 +0100

    Update report.js

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -31,10 +31,10 @@ const embed = new MessageEmbed()
 .setTimestamp()
 .setFooter(message.guild.name, message.guild.iconURL())
 .setAuthor("Reported member", rMember.user.displayAvatarURL())
-.setDescription(    `**Reported Member:** ${rMember}
-**Reported by:** ${message.member} (${message.member.id})
-**Reported in:** ${message.channel}
-**Reason:** ${args.slice(1).join(" ")}`)
+.setDescription(    `**Reported Member:** \`${rMember}\`
+**Reported by:** \`${message.member} (${message.member.id})\`
+**Reported in:** \`${message.channel}\`
+**Reason:** \`${args.slice(1).join(" ")}\``)
 
 channel.send(embed)
 
