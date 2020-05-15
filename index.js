50c84f66c0ec7133e5d8eef95ca77698b276cb36
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-15 01:35:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,9 +81,7 @@ client.on('message', message => {
      if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    const clientPerms = message.channel.permissionsFor(message.client.me)
 
-    if(!clientPerms.hasPermission("SEND_MESSAGES")) return;
 
     if(message.author.bot) return;
     
