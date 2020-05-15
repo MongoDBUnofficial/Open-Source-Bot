8201801385173bea2b4042d127782f2f5027a443
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-15 20:00:17 +0100

    Update notice.js

--- commands/moderation/notice.js
+++ commands/moderation/notice.js
@@ -4,8 +4,8 @@ const { stripIndents } = require("common-tags")
 module.exports = {
     name: "notice",
     category: "moderation",
-    description: "If the server has a channel correctly named 'notice-board' it will send what the user said in an embed, very similar to the announce command.",
-    usage: "c!announce <your message> (CAN INCLUDE MULTIPLE LINES WITH SHIFT + ENTER AND SUPPORTS MARKDOWN)",
+    description: "If the server has a channel correctly named 'notice-board' it will send what the user said in an embed, very similar to the announce command. It will only use here and not everyone.",
+    usage: "c!notice <your message> (CAN INCLUDE MULTIPLE LINES WITH SHIFT + ENTER AND SUPPORTS MARKDOWN)",
     run: async ( client,message, args) => {
 
         if (message.deletable) message.delete();
@@ -14,18 +14,18 @@ module.exports = {
 
 
 if (!message.member.hasPermission("MANAGE_MESSAGES")) 
-  return  message.reply("You don't have permissions to announce!").then(msg => msg.delete({ timeout: 3000})) 
+  return  message.reply("You don't have permissions to send out a notice!").then(msg => msg.delete({ timeout: 3000})) 
 
 
 
 
 if (!args[1])
-return message.channel.send("Please announce something!").then(msg => msg.delete({ timeout: 3000}))
+return message.channel.send("Please set a notice!").then(msg => msg.delete({ timeout: 3000}))
 
-const channel = message.guild.channels.cache.find(channel => channel.name === "notice");
+const channel = message.guild.channels.cache.find(channel => channel.name === "notice-board");
 
 if (!channel)
-return message.channel.send("You need an announcements channel to announce with this bot! Create a channel named announcements.")
+return message.channel.send("You need a notice-board channel to set a notice with this bot! Create a channel named notice-board.")
 
 const embed = new MessageEmbed()
 .setColor(roleColor)
@@ -37,7 +37,7 @@ const embed = new MessageEmbed()
 
 channel.send(`@here`, embed)
 
-message.channel.send("Announcement made by user: " + message.author.username )
+message.channel.send("Notice made by user: " + message.author.username )
 
     }
 
