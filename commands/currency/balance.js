69806a9e85ba0aa0a5a891a88b71039d0786b130
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-17 21:56:36 +0100

    fix

--- commands/currency/balance.js
+++ commands/currency/balance.js
@@ -8,25 +8,7 @@ module.exports = {
     usage: "`c!balance [user]`",
     run: async ( client,message, args) => {
 
-
-        if(!args[0]) {
-            var user = message.author;
-        } else {
-            var user = message.mentions.users.first() || bot.users.cache.get(args[0]); 
-        }
-        
-        if(!money[user.id]) {
-            money[user.id] = {
-                name: user.tag,
-                money: 0
-            }
-            fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-                if(err) console.log(err);
-            });
-        }
-
-        return message.channel.send(`${user.username} has $${money[user.id].money}.`);
-        
+        message.reply("Command in development.")
 
     }
 }
