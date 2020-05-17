60c560bd60d336f1446bd9b3a55d983fb0d940b1
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-17 21:30:22 +0100

    s

---
+++ commands/currency/balance.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const money = require("../money.json");
+
+module.exports = {
+    name: "balance",
+    category: "currency",
+    description: "Replies with your Crypto Bot balance.",
+    usage: "`c!balance [user]`",
+    run: async ( client,message, args) => {
+
+
+        if(!args[0]) {
+            var user = message.author;
+        } else {
+            var user = message.mentions.users.first() || bot.users.cache.get(args[0]); 
+        }
+        
+        if(!money[user.id]) {
+            money[user.id] = {
+                name: user.tag,
+                money: 0
+            }
+            fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+                if(err) console.log(err);
+            });
+        }
+
+        return message.channel.send(`${user.username} has $${money[user.id].money}.`);
+        
+
+    }
+}
