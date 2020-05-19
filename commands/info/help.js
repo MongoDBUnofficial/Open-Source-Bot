82c90355283c834b2fd9370489b4e811ce95e99c
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-19 18:16:07 +0100

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -32,9 +32,9 @@ function getAll(client, message) {
 
 const info = client.categories
 .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n \`\`\`${commands(cat)}`)
-.reduce((string, category) => string + "```\n" + category)
+.reduce((string, category) => string + "\n" + category)
 
-return message.channel.send(embed.setDescription(`${info}`))
+return message.channel.send(embed.setDescription(`\`\`\`${info}`))
 
 }
 
