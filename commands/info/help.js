b1d3287b5fca6ca651380c6925496dafea03695a
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-19 16:58:59 +0100

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -34,7 +34,7 @@ const info = client.categories
 .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n ${commands(cat)}`)
 .reduce((string, category) => string + "\n" + category)
 
-return message.channel.send(embed.setDescription(info))
+return message.channel.send(embed.setDescription(```\`\`${info}\`\`\``))
 
 }
 
