0403e74fbdaf6db1651d1522a1d3ddcbae433009
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-22 16:36:00 +0100

    Create ask.js

---
+++ commands/🎉 Fun 🎉/ask.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "8ball",
+    category: "🎉 Fun 🎉",
+    description: "Responds with a random yes/no answer to your question.",
+    usage: "`c!8ball <question>`",
+    run: async ( client,message, args) => {
+
+        message.reply("This command has been renamed to c!8ball. Try it!")
+
+    }
+}
