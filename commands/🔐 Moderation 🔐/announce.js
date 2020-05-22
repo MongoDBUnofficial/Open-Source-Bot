137da0a491b0b106ce340678d703f033468afd1e
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-22 19:00:05 +0100

    Update announce.js

--- commands/🔐 Moderation 🔐/announce.js
+++ commands/🔐 Moderation 🔐/announce.js
@@ -15,7 +15,7 @@ module.exports = {
 if (!message.member.hasPermission("MANAGE_MESSAGES")) 
   return  message.reply("You don't have permissions to announce!").then(msg => msg.delete({ timeout: 3000})) 
 
-  if(!args[0].includes(/^<#(\d+)>$/)) {
+  if(!args[0].match(/^<#(\d+)>$/)) {
   return message.channel.send(`Please use the correct format!
   **Usage:** c!announce <channel> <announcement>`).then(msg => msg.delete({ timeout: 5000})) 
   }
