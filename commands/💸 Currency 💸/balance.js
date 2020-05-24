dea574c1b944c024978de527cf5a03d4f8255321
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-24 16:21:33 +0100

    Create balance.js

---
+++ commands/💸 Currency 💸/balance.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "balance",
+    category: "💸 Currency 💸",
+    description: "Your crypto coins balance.",
+    usage: "`c!balance`",
+    run: async ( client,message, args) => {
+    
+        message.reply("In Construction")
+
+    }
+
+}
