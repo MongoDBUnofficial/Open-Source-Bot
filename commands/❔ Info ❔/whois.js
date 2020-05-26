665a5f065fd58d6f90b29f3986488819f9457a75
j-j-j-josh <39215410+j-j-j-josh@users.noreply.github.com>
2020-05-26 18:52:39 +0100

    Update whois.js

--- commands/❔ Info ❔/whois.js
+++ commands/❔ Info ❔/whois.js
@@ -35,7 +35,7 @@ module.exports = {
         .addField('Server', `\`\`\`${message.guild.name}\`\`\``)
         .addField('Nickname', `\`\`\`${member.nickname ? member.nickname : 'No Nickname'}\`\`\``)
         .addField('Status', `\`\`\`${status}\`\`\``)
-        .addField('Created At', `\`\`\`${whoismember.createdAt.toLocaleDateString()}\`\`\``, true)
+        .addField('Created At', `\`\`\`${usercreated}\`\`\``, true)
         .addField('Joined At', `\`\`\`${member.joinedAt.toLocaleDateString()}\`\`\``, true)
         .setFooter(message.guild.name, message.guild.iconURL())
         .setThumbnail(whoismember.displayAvatarURL({dynamic: true}))
